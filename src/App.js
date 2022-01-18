6ec6d69061debdd26db723c2bc479826733cc957
Megan Logsdon <meganlogsdon@Blakes-MacBook-Pro.local>
2022-01-18 17:51:27 -0500

    Adding fetch to App.js to retrieve data from Pokemon API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+  componentDidMount() {
+    // grab data from the api using AJAX
+    fetch("https://pokeapi.co/api/v2/pokemon")
+      .then(res => res.json())
+      .then(
+        (response) => {
+          console.log(response);
+          this.setState({
+            isLoaded: true,
+            items: response.results
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  render() {
+    const { error, isLoaded, items } = this.state;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {items.map((item, i) => <p key={i}>Here</p>)}
+        </header>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
