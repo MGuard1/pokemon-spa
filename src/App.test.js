5c2a34c40c2d5b7d4bba4d2667900675269acae8
Megan Logsdon <meganlogsdon@Blakes-MacBook-Pro.local>
2022-01-20 14:25:27 -0500

    finalizing tests

--- src/App.test.js
+++ src/App.test.js
@@ -15,32 +15,32 @@ test('renders App component', async () => {
 
 test('favorites button adds item to favorites and removes item from favorites', async () => {
 
-  const doc = render(<App />);
+  const { getAllByTestId, queryAllByTestId } = render(<App />);
 
   await waitFor(() => expect(screen.getByText("bulbasaur")).toBeInTheDocument());
 
-  fireEvent.click(doc.getAllByTestId('favorite')[0], {bubbles: true});
+  fireEvent.click(getAllByTestId('favorite')[0], {bubbles: true});
 
   // check if favorites item now displays in the favorites container
-  expect(doc.queryAllByTestId('favorite-item').length).toBe(1);
+  expect(queryAllByTestId('favorite-item').length).toBe(1);
 
   // check if favorites item is then removed by clicking X
-  fireEvent.click(doc.getAllByTestId('unfavorite')[0], {bubbles: true});
+  fireEvent.click(getAllByTestId('unfavorite')[0], {bubbles: true});
 
-  expect(doc.queryAllByTestId('favorite-item').length).toBe(0);
+  expect(queryAllByTestId('favorite-item').length).toBe(0);
 
 });
 
 test('sort alphabetically', async () => {
 
-  const doc = render(<App />);
+  const { getAllByTestId } = render(<App />);
 
   await waitFor(() => expect(screen.getByText("bulbasaur")).toBeInTheDocument());
   expect(screen.getByText('Sort Alphabetically')).toBeInTheDocument();
 
   fireEvent.click(screen.getByText('Sort Alphabetically'));
 
-  expect(doc.getAllByTestId('pokemon-name')[0]).toHaveTextContent('beedrill');
+  expect(getAllByTestId('pokemon-name')[0]).toHaveTextContent('beedrill');
 
 });
 
