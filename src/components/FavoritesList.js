e40bb4f76dcd50c2b173c5931b81bfc20d27ec79
Megan Logsdon <meganlogsdon@Blakes-MacBook-Pro.local>
2022-01-21 09:48:59 -0500

    adding titles to buttons

--- src/components/FavoritesList.js
+++ src/components/FavoritesList.js
@@ -11,7 +11,11 @@ function FavoritesList(props) {
             {props.list.map((item, id) => {
             return (
             <li key={id} className="favorite-item" data-testid="favorite-item">
-                <button aria-label={`remove ${item.name} from favorites`} onClick={() => props.addFavorite(item)}> X </button>
+                <button 
+                  aria-label={`remove ${item.name} from favorites`} 
+                  title={`remove ${item.name} from favorites`} 
+                  onClick={() => props.addFavorite(item)}> 
+                  X </button>
                 <p>{item.name}</p>
             </li>
             );
